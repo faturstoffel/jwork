9272ba1011a5b25efaaf576247c988b20f02a2da
fatur <faturrahmanstoffel90@gmail.com>
2021-05-08 09:39:37 +0700

    Modul 7 - Post Test

--- src/JWork.java
+++ src/JWork.java
@@ -1,4 +1,5 @@
 import java.util.GregorianCalendar;
+import java.util.ArrayList;
 
 /**
  * Pada class JWork ini bisa digunakan untuk memasukkan input ke object melalui beberapa baris code
@@ -13,67 +14,41 @@ public class JWork
 
     public static void main(String[] args)
     {
+        ArrayList<Job> jobs = new ArrayList<Job>();
+        ArrayList<Job> jobs1 = new ArrayList<Job>();
+
         try {
-            DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId() + 1, "Fatur Stoffel",
-                    "fatur@ui.ac.id", "hacker23", 2022, 05, 06));
-        } catch (EmailAlreadyExistsException e) {
-            System.out.println(e.getMessage());
+            jobs.add(new Job(1, "Backend Engineer", DatabaseRecruiter.getRecruiterById(1), 1000, JobCategory.BackEnd));
+        }
+        catch (RecruiterNotFoundException e) {
+            e.printStackTrace();
         }
         try {
-            DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId() + 1, "Fatur Rahman",
-                    "dimas@ui.ac.id", "dimas23", 2022, 03, 06));
-        } catch (EmailAlreadyExistsException e) {
-            System.out.println(e.getMessage());
+            jobs1.add(new Job(2, "Frontend Engineer", DatabaseRecruiter.getRecruiterById(1), 1000, JobCategory.FrontEnd));
+        }
+        catch (RecruiterNotFoundException e) {
+            e.printStackTrace();
         }
         try {
-            DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId() + 1, "haru",
-                    "kemal@ui.ac.id", "kemalgaming", 2023, 05, 04));
-        } catch (EmailAlreadyExistsException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            DatabaseJobseeker.addJobseeker(new Jobseeker(DatabaseJobseeker.getLastId() + 1, "emas",
-                    "farrel@ui.ac.id", "farrel69", 2022, 03, 06));
-        } catch (EmailAlreadyExistsException e) {
+            DatabaseInvoice.addInvoice(new EwalletPayment(1, jobs, DatabaseJobseeker.getJobseekerById(1)));
+        } catch (JobseekerNotFoundException | OngoingInvoiceAlreadyExistsException e) {
             System.out.println(e.getMessage());
         }
 
         try {
-            DatabaseBonus.addBonus(new Bonus(101, "BreathItIn", 200000, 500000, false));
-        } catch (ReferralCodeAlreadyExistsException e) {
+            DatabaseInvoice.addInvoice(new EwalletPayment(2, jobs1, DatabaseJobseeker.getJobseekerById(2)));
+        } catch (JobseekerNotFoundException | OngoingInvoiceAlreadyExistsException e) {
             System.out.println(e.getMessage());
         }
-        try {
-            DatabaseBonus.addBonus(new Bonus(101, "BreathItIn", 200000, 500000, true));
-        } catch (ReferralCodeAlreadyExistsException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println("=========Database JobSeeker============");
-        System.out.println(DatabaseJobseeker.getDatabaseJobseeker());
 
-        System.out.println("=========Database Bonus===============");
-        System.out.println(DatabaseBonus.getBonusDatabase());
-        try {
-            DatabaseJobseeker.removeJobseeker(24);
-        } catch (JobSeekerNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            DatabaseRecruiter.removeRecruiter(23);
-        } catch (RecruiterNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            DatabaseJob.getJobById(20);
-        } catch (JobNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            DatabaseBonus.getBonusById(21);
-        } catch (BonusNotFoundException bonusNotFound) {
-            System.out.println(bonusNotFound.getMessage());
+        System.out.println("\n ========== Invoice ==========");
+        System.out.println(DatabaseInvoice.getInvoiceDatabase());
+
+        for (Invoice invoice : DatabaseInvoice.getInvoiceDatabase()){
+            new Thread(new FeeCalculator(invoice)).start();
         }
     }
+}
 
 
        
